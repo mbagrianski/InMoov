045db3016d4ae28f8417ea959d5cab25e18c5bd7
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2020-01-10 18:45:39 -0500

    Update NewPanel.ino

--- NewPanel.ino
+++ NewPanel.ino
@@ -14,22 +14,28 @@ void setup() {
   Serial.begin(9600); //Starts Serial Com
   Wire.begin(); //Begins I2C communication at pin (A4,A5)
   lcd.begin(16, 2);
-  lcd.print("hello, world!");
+
 }
 
 void loop() {
   updateSensor();
+  lcd.setCursor(0, 0);
+  lcd.print("UP=motor_sel0-11");
+  lcd.setCursor(0, 1);
+  lcd.print("Motor: ");
+  lcd.print(motor);
+
   if (sensor > 1000) {
 
   } else if (sensor > 700) {
     lcd.clear();
     lcd.print("select");
     delay(200);
+    setMotor();
   } else if (sensor > 500) {
     lcd.clear();
     lcd.print("left");
     delay(200);
-    switchMotor();
   } else if (sensor > 300) {
     lcd.clear();
     lcd.print("down");
@@ -37,88 +43,102 @@ void loop() {
   } else if (sensor > 100) {
     lcd.clear();
     lcd.print("up");
-    delay(200);
+    //delay(200);
+    switchMotor();
   } else {
     lcd.clear();
     lcd.print("right");
     delay(200);
+    Serial.println(sensor);
   }
 }
 
 void switchMotor() {
-  if (motor < 5) {
+  if (motor < 11) {
     motor++;
   } else {
     motor = 0;
   }
+  delay(200);
 }
 
-void setMotor(int motor, double pos) {
+void setMotor() {
   updateSensor();
-    
+
   while (sensor < 700 || sensor > 1000) {
-    
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Motor ");
     lcd.print(motor);
     lcd.print(" pos -/+");
-  
-    if (sensor > 500) { //left      
-      lcd.setCursor(0, 1);
-      decrementMotor();
-      lcd.print(motorVal);
-    } else if (sensor > 300) { //down
-      
-    } else if (sensor > 100) { //up
-      
-    } else { //right
-      lcd.setCursor(0, 1);
-      incrementMotor();
-      lcd.print(motorVal);
-    }    
-    updateSensor();    
-  }
 
-  if(sensor > 700){
-    sendMsg(motor, motorVal);
+    if (sensor > 500 && sensor < 700) { //left
+      decrementMotor();
+    } else if (sensor > 300) { //down
+
+    } else if (sensor > 100) { //up
+
+    } else { //right
+      incrementMotor();
+    }
+    lcd.setCursor(0, 1);
+    lcd.print("Degrees ");
+    lcd.print(motorVal);
+
+    updateSensor();
+    delay(140);
   }
+  sendMsg();
+
 }
 
-void incrementMotor(){
-  if(motorVal <= 180){
+void incrementMotor() {
+  if (motorVal <= 180) {
     motorVal++;
-  }else{
+  } else {
     error("Ex_lim+");
   }
+  delay(10);
 }
 
-void decrementMotor(){
-  if(motorVal >= 0){
+void decrementMotor() {
+  if (motorVal >= 0) {
     motorVal--;
-  }else{
+  } else {
     error("Ex_lim-");
   }
+  delay(10);
 }
 
-void error(String message){
+void error(String message) {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Error : ");
-  lcd.print(message);  
+  lcd.print(message);
   delay(1000);
 }
 
-void updateSensor(){
+void updateSensor() {
   sensor = analogRead(analogPin);
   Serial.println(sensor);
+  delay(10);
 }
 
-void sendMsg(int motor, double motorVal){
+void sendMsg() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Sending motor ");
+  lcd.print(motor);
+  lcd.setCursor(0, 1);
+  lcd.print(" ");
+  lcd.print(motorVal);
+  lcd.print(" deg");
   byte x = motor;
   byte y = motorVal;
-  Wire.beginTransmission(8); 
-  Wire.write(x); 
-  Wire.write(y); 
+  Wire.beginTransmission(8);
+  Wire.write(x);
+  Wire.write(y);
   Wire.endTransmission();
+  delay(1000);
+  lcd.clear();
 }
